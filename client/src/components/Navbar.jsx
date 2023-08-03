5aeb2b2e2b22faf73d80d233b50fbd5d938f9830
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-03 02:51:07 -0400

    Added Dark Mode Toggle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import logo from "../assets/sleek-walk-logo.png"
 
 const Navbar = () => {
+
+  // Used for the light/dark mode
+  // use theme from local storage if available or set light theme
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  // update state on toggle
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  // set theme state in localstorage on mount & also update localstorage on state change
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    // add custom data-theme attribute to html tag required to update theme using DaisyUI
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+
   return (
     <div className="bg-base-200 py-2">
       <div className="max-w-7xl mx-auto navbar">
   
+        {/* Small Screen Menu-Logo Section */}
         <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -40,7 +66,16 @@ const Navbar = () => {
                 <li>
                   <Link to="/">Kids</Link>
                 </li>
-        
+
+                <div className="divider"></div>
+
+                <li>
+                  <Link to="/">Login</Link>
+                </li>
+                <li>
+                  <Link to="/">Signup</Link>
+                </li>
+
                 <li>
                   <div className="form-control mt-3">
                     <input
@@ -60,6 +95,7 @@ const Navbar = () => {
             </Link>
         </div>
 
+        {/* Menu Section */}
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
             <li class="text-xl">
@@ -74,7 +110,10 @@ const Navbar = () => {
           </ul>
         </div>
 
+        {/* Search-Cart-User Section */}
         <div className="navbar-end hidden lg:flex ">
+
+          {/* Search */}
           <div className="form-control p-2 ">
             <input
               type="text"
@@ -82,9 +121,8 @@ const Navbar = () => {
               className="input input-bordered input-primary w-full"
             />
           </div>
-        </div>
 
-        <div className="flex justify-end flex-1 px-2">
+          {/* Cart */}
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <div className="indicator">
@@ -113,6 +151,8 @@ const Navbar = () => {
               </div>
             </div>
           </div>
+
+          {/* User */}
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
             <svg
@@ -140,6 +180,29 @@ const Navbar = () => {
               <li><a href="/">Sign up</a></li>
             </ul>
           </div>
+        </div>
+
+        {/* Light/Dark Mode Section */}
+        <div className="flex justify-end flex-1 px-2">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <label className="swap swap-rotate">
+              <input
+                type="checkbox"
+                onChange={handleToggle}
+                // show toggle image based on localstorage theme
+                checked={theme === "light" ? false : true}
+              />
+              {/* this hidden checkbox controls the state */}
+              <input type="checkbox" />
+              
+              {/* sun icon */}
+              <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+              
+              {/* moon icon */}
+              <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+              
+            </label>
+          </label>
         </div>
 
       </div>
