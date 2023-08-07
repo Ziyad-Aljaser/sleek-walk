7cb8365060ed5b3f065ef2bf7ffa5c9ba05d8ca8
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-07 06:51:23 -0400

    Update Hero section as components

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import hero_img from "../assets/hero_img.png"
+
+
+const Hero = () => {
+
+    return (
+        <div className="hero py-16 sm:py-32 bg-base-300">
+            <div className="hero-content grid md:grid-cols-2 gap-8">
+            <div className="order-2">
+                <img src={hero_img} className="" alt="Hero" />
+            </div>
+            <div>
+                <h1 className="text-5xl font-bold leading-normal">
+                Welcome to Sleek Walk Online Shoes Store
+                </h1>
+                <p className="py-6">
+                With us, you can shop online & help save your street at the same
+                time. If you’re looking for great value shoes, then
+                Sleek Walk is the place for you. As you may already know, we
+                aren’t like other online shoes stores.
+                </p>
+                <button className="btn btn-primary mt-4">Explore Shoes</button>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-
-import hero_img from "../assets/hero_img.png"
+import Hero from "../components/Hero";
 
 
 export default function Home() {
@@ -11,25 +10,7 @@ export default function Home() {
     <Layout>
       {/* Hero Section */}
       {/* The padding is smaller when the screen is small */}
-      <div className="hero py-16 sm:py-32 bg-base-300">
-        <div className="hero-content grid md:grid-cols-2 gap-8">
-          <div className="order-2">
-            <img src={hero_img} className="" alt="Hero" />
-          </div>
-          <div>
-            <h1 className="text-5xl font-bold leading-normal">
-              Welcome to Sleek Walk Online Shoes Store
-            </h1>
-            <p className="py-6">
-              With us, you can shop online & help save your street at the same
-              time. If you’re looking for great value shoes, then
-              Sleek Walk is the place for you. As you may already know, we
-              aren’t like other online shoes stores.
-            </p>
-            <button className="btn btn-primary mt-4">Explore Shoes</button>
-          </div>
-        </div>
-      </div>
+      <Hero />
 
     </Layout>
   );
