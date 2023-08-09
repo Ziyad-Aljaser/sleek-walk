ec00551c790eda8e041a97e196e9a8ed8fcc816b
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-09 13:50:58 -0400

    fix the width of login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
     <Layout>
 
         <div className="flex items-center justify-center h-screen bg-base-300 p-5"> {/* Centering container */}
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-full max-w-md bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h1 className="text-4xl font-bold text-center mb-6">Login</h1>
                     <div className="form-control">
