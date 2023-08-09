002bee56e50eef17e15f055d2b1437760a40dd3e
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-09 08:06:18 -0400

    Scroll to top of the page whenever route changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import { Route, Routes } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ShoeDetails from "./pages/ShoeDetails";
 
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/shoes-details/:id" element={<ShoeDetails />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/shoes-details/:id" element={<ShoeDetails />} />
+      </Routes>
+    </>
   );
 }
 
