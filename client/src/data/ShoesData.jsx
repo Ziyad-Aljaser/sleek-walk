5739d8e1e573d15daf3d4778356ec86f509aec8d
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-09 18:30:14 -0400

    Update the images location

--- client/src/data/ShoesData.jsx
+++ client/src/data/ShoesData.jsx
@@ -1,52 +1,80 @@
-import shoes1 from "../assets/shoes1.png"
-import shoes2 from "../assets/shoes2.png"
-import shoes3 from "../assets/shoes3.png"
-import men_casual_shoes1 from "../assets/men_casual_shoes_1.png"
-import men_casual_shoes2 from "../assets/men_casual_shoes_2.png"
-import men_casual_shoes3 from "../assets/men_casual_shoes_3.png"
+import men_athletic_shoes_1 from "../assets/Shoes_Images/men_athletic_shoes_1.png"
+import men_athletic_shoes_2 from "../assets/Shoes_Images/men_athletic_shoes_2.png"
+import men_athletic_shoes_3 from "../assets/Shoes_Images/men_athletic_shoes_3.png"
+
+import men_casual_shoes_1 from "../assets/Shoes_Images/men_casual_shoes_1.png"
+import men_casual_shoes_2 from "../assets/Shoes_Images/men_casual_shoes_2.png"
+import men_casual_shoes_3 from "../assets/Shoes_Images/men_casual_shoes_3.png"
+
+import men_dress_shoes_1 from "../assets/Shoes_Images/men_dress_shoes_1.png"
+import men_dress_shoes_2 from "../assets/Shoes_Images/men_dress_shoes_2.png"
+import men_dress_shoes_3 from "../assets/Shoes_Images/men_dress_shoes_3.png"
+
+
 
 // Shoes Data
 export const SHOES = [
     {
       id: 1,
-      image: shoes1,
-      title: "Sport Shoes 1",
-      category: "Sport",
+      image: men_athletic_shoes_1,
+      title: "Athletic Shoes 1",
+      category: "Athletic",
       price: "$40",
     },
     {
       id: 2,
-      image: shoes2,
-      title: "Sport Shoes 2",
-      category: "Sport",
+      image: men_athletic_shoes_2,
+      title: "Athletic Shoes 2",
+      category: "Athletic",
       price: "$70.99",
     },
     {
       id: 3,
-      image: shoes3,
-      title: "Sport Shoes 3",
-      category: "Sport",
+      image: men_athletic_shoes_3,
+      title: "Athletic Shoes 3",
+      category: "Athletic",
       price: "$58",
     },
     {
       id: 4,
-      image: men_casual_shoes1,
+      image: men_casual_shoes_1,
       title: "Casual Shoes 1",
       category: "Casual",
       price: "$53",
     },
     {
       id: 5,
-      image: men_casual_shoes2,
+      image: men_casual_shoes_2,
       title: "Casual Shoes 2",
       category: "Casual",
       price: "$68",
     },
     {
       id: 6,
-      image: men_casual_shoes3,
+      image: men_casual_shoes_3,
       title: "Casual Shoes 3",
       category: "Casual",
       price: "$98",
+    },
+    {
+      id: 7,
+      image: men_dress_shoes_1,
+      title: "Dress Shoes 1",
+      category: "Dress",
+      price: "$298",
+    },
+    {
+      id: 8,
+      image: men_dress_shoes_2,
+      title: "Dress Shoes 2",
+      category: "Dress",
+      price: "$198",
+    },
+    {
+      id: 9,
+      image: men_dress_shoes_3,
+      title: "Dress Shoes 3",
+      category: "Dress",
+      price: "$318",
     },
   ];
