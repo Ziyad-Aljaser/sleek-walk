7d54f3b0dbd59e9b8399666c15ae76d1e727bf01
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-09 06:50:51 -0400

    Added Shoes Data

---
+++ client/src/data/ShoesData.jsx
@@ -0,0 +1,52 @@
+import shoes1 from "../assets/shoes1.png"
+import shoes2 from "../assets/shoes2.png"
+import shoes3 from "../assets/shoes3.png"
+import men_casual_shoes1 from "../assets/men_casual_shoes_1.png"
+import men_casual_shoes2 from "../assets/men_casual_shoes_2.png"
+import men_casual_shoes3 from "../assets/men_casual_shoes_3.png"
+
+// Shoes Data
+export const SHOES = [
+    {
+      id: 1,
+      image: shoes1,
+      title: "Sport Shoes 1",
+      category: "Sport",
+      price: "$40",
+    },
+    {
+      id: 2,
+      image: shoes2,
+      title: "Sport Shoes 2",
+      category: "Sport",
+      price: "$70.99",
+    },
+    {
+      id: 3,
+      image: shoes3,
+      title: "Sport Shoes 3",
+      category: "Sport",
+      price: "$58",
+    },
+    {
+      id: 4,
+      image: men_casual_shoes1,
+      title: "Casual Shoes 1",
+      category: "Casual",
+      price: "$53",
+    },
+    {
+      id: 5,
+      image: men_casual_shoes2,
+      title: "Casual Shoes 2",
+      category: "Casual",
+      price: "$68",
+    },
+    {
+      id: 6,
+      image: men_casual_shoes3,
+      title: "Casual Shoes 3",
+      category: "Casual",
+      price: "$98",
+    },
+  ];
