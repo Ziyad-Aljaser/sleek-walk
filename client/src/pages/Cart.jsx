19f5a68a413fc755178d38f1f7e23246accbdd9a
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-14 14:07:28 -0400

    Update payment section

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,30 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Layout from "../components/Layout";
 
-import men_dress_shoes_1 from "../assets/Shoes_Images/men_dress_shoes_1.png"
+import men_dress_shoes_1 from "../assets/Shoes_Images/men_dress_shoes_1.png";
 
 export default function Cart() {
   // Sample data for demonstration
   const [products, setProducts] = useState([
-    { product: "Apple", price: 1.2, qty: 4, total: 4.8, img: men_dress_shoes_1 },
-    { product: "Banana", price: 0.8, qty: 5, total: 4.0, img: men_dress_shoes_1 },
-    { product: "Cherry", price: 2.5, qty: 3, total: 7.5, img: men_dress_shoes_1 },
+    {
+      product: "Apple",
+      price: 1.2,
+      qty: 4,
+      total: 4.8,
+      img: men_dress_shoes_1,
+    },
+    {
+      product: "Banana",
+      price: 0.8,
+      qty: 5,
+      total: 4.0,
+      img: men_dress_shoes_1,
+    },
+    {
+      product: "Cherry",
+      price: 2.5,
+      qty: 3,
+      total: 7.5,
+      img: men_dress_shoes_1,
+    },
   ]);
 
   // Delete Button Handler
   const handleDelete = (productName) => {
-    const updatedProducts = products.filter(p => p.product !== productName);
+    const updatedProducts = products.filter((p) => p.product !== productName);
     setProducts(updatedProducts);
-    console.log("Deleted Successfully")
-  }
+    console.log("Deleted Successfully");
+  };
 
   return (
     <Layout>
       <div className="p-2 sm:p-16 bg-base-300">
         <table className="table w-full">
-
           {/* Table Header */}
           <thead>
             <tr>
@@ -39,12 +56,11 @@ export default function Cart() {
           <tbody>
             {products.map((product, index) => (
               <tr key={index}>
-
                 {/* Product image with the delete button */}
                 <td className="py-8">
                   <div className="indicator z-[0]">
-                    <span 
-                      className="indicator-item badge badge-secondary cursor-pointer" 
+                    <span
+                      className="indicator-item badge badge-secondary cursor-pointer"
                       onClick={() => handleDelete(product.product)} // Using product name to identify which product to delete
                     >
                       ✕
@@ -60,19 +76,17 @@ export default function Cart() {
                 <td>${product.price.toFixed(2)}</td>
                 <td>{product.qty}</td>
                 <td>${product.total.toFixed(2)}</td>
-
               </tr>
             ))}
           </tbody>
-
         </table>
 
         <div className="flex justify-center">
-          <button className="btn btn-primary w-64"><Link to="/checkout">Checkout</Link></button>
+          <button className="btn btn-primary w-64">
+            <Link to="/checkout">Proceed to Checkout</Link>
+          </button>
         </div>
-
       </div>
     </Layout>
   );
-
 }
--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import Layout from "../components/Layout";
 
@@ -6,158 +6,220 @@ export default function Signup() {
   const [step, setStep] = useState(0);
 
   const nextStep = () => {
-      if (step < 3) {
-          setStep(step + 1);
-      }
+    if (step < 3) {
+      setStep(step + 1);
+    }
   };
 
   const prevStep = () => {
-      if (step > 0) {
-          setStep(step - 1);
-      }
+    if (step > 0) {
+      setStep(step - 1);
+    }
   };
+
+  // Regular expression to match non-alphabetic characters
+  const handleNameChange = (event) => {
+    const nonAlphabetical = /[^A-Za-z\s]/g;
+    event.target.value = event.target.value.replace(nonAlphabetical, '');
+  };
+
+  // Regular expression to remove non-numeric characters
+  const handleCreditCardChange = (event) => {
+    const nonNumeric = /[^0-9]/g;
+    let value = event.target.value.replace(nonNumeric, '');
+  
+    // Add a space after every 4 digits
+    value = value.replace(/\D/g, '').replace(/(\d{4})(?=\d)/g, '$1 ');
+  
+    event.target.value = value;
+  };
+
+    // Regular expression to remove non-numeric characters and existing slashes
+    const handleExpirationDateChange = (event) => {
+      // Regular expression to remove non-numeric characters and existing slashes
+      const nonNumeric = /[^0-9]/g;
+      let value = event.target.value.replace(nonNumeric, '');
+    
+      // If the first character is bigger than 2, prepend with 0
+      if (value.length === 1 && parseInt(value) > 2) {
+        value = '0' + value;
+      }
+    
+      // If more than two characters, add a '/' after the 2nd digit
+      if (value.length > 2) {
+        value = value.substring(0, 2) + '/' + value.substring(2, 4);
+      }
+    
+      event.target.value = value;
+    };
 
   return (
     <Layout>
-        <div className="flex items-center justify-center h-screen bg-base-300 p-5">
-            <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center min-h-screen bg-base-300 p-5">
+        <div className="flex flex-col items-center">
+          {/* Steps Section */}
+          <ul className="steps mb-12 z-[0]">
+            <li className={step >= 0 ? "step step-primary" : "step"}>
+              Shipping Address
+            </li>
+            <li className={step >= 1 ? "step step-primary" : "step"}>
+              Payment
+            </li>
+            <li className={step >= 2 ? "step step-primary" : "step"}>
+              Review & Confirm
+            </li>
+          </ul>
 
-                {/* Steps Section */}
-                <ul className="steps mb-12 z-[0]">
-                    <li className={step >= 0 ? "step step-primary" : "step"}>Personal Information</li>
-                    <li className={step >= 1 ? "step step-primary" : "step"}>Password</li>
-                    <li className={step >= 2 ? "step step-primary" : "step"}>Address</li>
-                    <li className={step >= 3 ? "step step-primary" : "step"}>Review & Confirm</li>
-                </ul>
+          <div className="card bg-base-100 shadow-xl w-full max-w-md">
+            <div className="card-body">
 
-                <div className="card bg-base-100 shadow-xl w-full max-w-md">
-                    <div className="card-body">
-                        <h1 className="text-4xl font-bold text-center mb-6">CheckOut</h1>
-                        
-                        {/* Address Section */}
-                        {step === 0 && (
-                            <div>
-                                {/* Counrty */}
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Country</span>
-                                    </label>
-                                    <select className="select select-bordered select-primary w-full">
-                                        <option disabled selected>Select a country</option>
-                                        <option value="CAN">Canada</option>
-                                        <option value="KSA">Saudi Arabia</option>
-                                        <option value="GBR">United Kingdom</option>
-                                        <option value="USA">United States</option>
-                                    </select>
-                                </div>
+              <h1 className="text-4xl font-bold text-center mb-6">Checkout</h1>
 
-                                {/* City */}
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">City</span>
-                                    </label>
-                                    <input type="text" placeholder="City" className="input input-bordered input-primary" />
-                                </div>
+              {/* Address Section */}
+              {step === 0 && (
+                <div>
+                  {/* Counrty */}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Country</span>
+                    </label>
+                    <select className="select select-bordered select-primary w-full">
+                      <option disabled selected>
+                        Select a country
+                      </option>
+                      <option value="CAN">Canada</option>
+                      <option value="KSA">Saudi Arabia</option>
+                      <option value="GBR">United Kingdom</option>
+                      <option value="USA">United States</option>
+                    </select>
+                  </div>
 
-                                {/* Street */}
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Street</span>
-                                    </label>
-                                    <input type="text" placeholder="Street" className="input input-bordered input-primary" />
-                                </div>
+                  {/* City */}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">City</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="city"
+                      className="input input-bordered input-primary"
+                    />
+                  </div>
 
-                                <div className="form-control mt-8">
-                                    <button onClick={nextStep} className="btn btn-primary">Next</button>
-                                </div>
+                  {/* Street */}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Street</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="street"
+                      className="input input-bordered input-primary"
+                    />
+                  </div>
 
-                            </div>
-                        )}
-
-                        {/* Password Secion */}
-                        {step === 1 && (
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="password" className="input input-bordered input-primary" />
-                                
-                                <label className="label">
-                                    <span className="label-text">Confirm Password</span>
-                                </label>
-                                <input type="password" placeholder="confirm password" className="input input-bordered input-primary" />
-                            </div>
-                        )}
-                        
-                        {/* Address Section */}
-                        {step === 2 && (
-                            <div>
-                                {/* Counrty */}
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Country</span>
-                                    </label>
-                                    <select className="select select-bordered select-primary w-full">
-                                        <option disabled selected>Select a country</option>
-                                        <option value="CAN">Canada</option>
-                                        <option value="KSA">Saudi Arabia</option>
-                                        <option value="GBR">United Kingdom</option>
-                                        <option value="USA">United States</option>
-                                    </select>
-                                </div>
-
-                                {/* City */}
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">City</span>
-                                    </label>
-                                    <input type="text" placeholder="City" className="input input-bordered input-primary" />
-                                </div>
-
-                                {/* Street */}
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Street</span>
-                                    </label>
-                                    <input type="text" placeholder="Street" className="input input-bordered input-primary" />
-                                </div>
-                            </div>
-                        )}
-                        
-                        {/* Review & Confirm */}
-                        {step === 3 && (
-                            <div>
-                                <p className="text-center mb-6">Review your details and confirm</p>
-
-                                <label className="label">
-                                    <span className="label-text">First Name</span>
-                                </label>
-                                <input type="fname" placeholder="first name" className="input input-bordered w-full max-w-xs" disabled />
-
-                                <label className="label">
-                                    <span className="label-text">last Name</span>
-                                </label>
-                                <input type="lname" placeholder="last name" className="input input-bordered w-full max-w-xs" disabled />
-                                
-                                <div className="form-control mt-8">
-                                    <button className="btn btn-primary">Sign Up</button>
-                                </div>
-                            </div>
-                        )}
-
-                        {/* Next/Back Section */}
-                        {step > 0 && step < 3 && (
-                            <div className="form-control mt-6 flex flex-row justify-between">
-                                <button onClick={prevStep} className="btn btn-primary w-1/4">Back</button>
-                                <button onClick={nextStep} className="btn btn-primary w-1/4">Next</button>
-                            </div>
-                        )}
-
-                    </div>
+                  <div className="form-control mt-8">
+                    <button onClick={nextStep} className="btn btn-primary">
+                      Next
+                    </button>
+                  </div>
                 </div>
-            </div>
-        </div>
+              )}
 
+              {/* Payment Secion */}
+              {step === 1 && (
+                <div className="form-control">
+
+                  <label className="label">
+                    <span className="label-text">Cardholder Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="cardholder name"
+                    className="input input-bordered input-primary"
+                    onChange={handleNameChange}
+                  />
+
+                  <label className="label mt-5">
+                    <span className="label-text">Credit Card Information</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="**** **** **** ****"
+                    className="input input-bordered input-primary"
+                    onChange={handleCreditCardChange}
+                    maxLength={19} // 16 digits + 3 spaces
+                  />
+
+                  <div className="flex flex-col sm:flex-row">
+                    <input
+                      type="text"
+                      placeholder="MM/YY"
+                      className="input input-bordered input-primary sm:w-1/4 mt-5 sm:mr-5"
+                      onChange={handleExpirationDateChange}
+                      maxLength="5"
+                    />
+                    <input
+                      type="password"
+                      placeholder="CVC"
+                      className="input input-bordered input-primary sm:w-1/4 mt-5"
+                      maxLength="3"
+                    />
+                  </div>
+
+                </div>
+              )}
+
+              {/* Review & Confirm */}
+              {step === 2 && (
+                <div>
+                  <p className="text-center mb-6">
+                    Review your details and confirm
+                  </p>
+
+                  <label className="label">
+                    <span className="label-text">First Name</span>
+                  </label>
+                  <input
+                    type="fname"
+                    placeholder="first name"
+                    className="input input-bordered w-full max-w-xs"
+                    disabled
+                  />
+
+                  <label className="label">
+                    <span className="label-text">last Name</span>
+                  </label>
+                  <input
+                    type="lname"
+                    placeholder="last name"
+                    className="input input-bordered w-full max-w-xs"
+                    disabled
+                  />
+
+                  <div className="form-control mt-8">
+                    <button className="btn btn-primary">Complete Order</button>
+                  </div>
+                </div>
+              )}
+
+              {/* Next/Back Section */}
+              {step > 0 && step < 2 && (
+                <div className="form-control mt-6 flex flex-row justify-between">
+                  <button onClick={prevStep} className="btn btn-primary w-1/4">
+                    Back
+                  </button>
+                  <button onClick={nextStep} className="btn btn-primary w-1/4">
+                    Next
+                  </button>
+                </div>
+              )}
+              
+            </div>
+          </div>
+
+        </div>
+      </div>
     </Layout>
   );
 }
