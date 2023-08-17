7b513e2049f823ae8c2c6077bf0798bd93b14896
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-08-17 08:44:51 -0400

    Add Filter button for men/women pages

--- client/src/pages/MenShop.jsx
+++ client/src/pages/MenShop.jsx
@@ -48,7 +48,7 @@ export default function MenShop() {
         <h1 className="text-4xl p-7 font-semibold">Men Shoes</h1>
 
         {/* Sort Section */}
-        <div className="flex justify-start w-full">
+        <div className="flex justify-start w-full ">
           <select
             className="select select-primary ml-10"
             onChange={handleSortChange}
@@ -61,33 +61,66 @@ export default function MenShop() {
           </select>
         </div>
 
-        {/* Shoes Cards Section */}
-        <Shoes
-          currentPage={currentPage}
-          itemsPerPage={itemsPerPage}
-          type="Men"
-          sortOrder={sortOrder}
-        />
-
-        {/* Pagination Section */}
-        {filteredShoes.length > itemsPerPage && (
-          <div className="join flex justify-center shadow-xl mb-5">
-            {[...Array(totalPages)].map((_, index) => (
-              <button
-                key={index + 1}
-                className={`join-item btn text-2xl ${
-                  currentPage === index + 1 ? "btn-active" : ""
-                }`}
-                onClick={() => {
-                  setCurrentPage(index + 1);
-                  handleButtonClick();
-                }}
+        {/* Drawer Section */}
+        <div className="flex justify-start w-full">
+          <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <label
+                htmlFor="my-drawer"
+                className="btn btn-primary drawer-button"
               >
-                {index + 1}
-              </button>
-            ))}
+                Filter
+              </label>
+              {/* Shoes Cards Section */}
+              <Shoes
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+                type="Men"
+                sortOrder={sortOrder}
+              />
+
+              {/* Pagination Section */}
+              {filteredShoes.length > itemsPerPage && (
+                <div className="join flex justify-center mb-5">
+                  {[...Array(totalPages)].map((_, index) => (
+                    <button
+                      key={index + 1}
+                      className={`join-item btn text-2xl ${
+                        currentPage === index + 1 ? "btn-active" : ""
+                      }`}
+                      onClick={() => {
+                        setCurrentPage(index + 1);
+                        handleButtonClick();
+                      }}
+                    >
+                      {index + 1}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            {/* Drawer Side Section */}
+            <div className="drawer-side">
+              <label htmlFor="my-drawer" className="drawer-overlay"></label>
+              <ul className="menu py-24 w-80 h-full bg-base-200 text-base-content">
+                <li>
+                  <a href="#">Sidebar Item 1</a>
+                </li>
+                <li>
+                  <a href="#">Sidebar Item 2</a>
+                </li>
+                <li>
+                  <a href="#">Sidebar Item 3</a>
+                </li>
+                <li>
+                  <a href="#">Sidebar Item 4</a>
+                </li>
+              </ul>
+            </div>
           </div>
-        )}
+        </div>
       </div>
     </Layout>
   );
