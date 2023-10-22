47a8acbf5fee456b8f1e0d2a11f7290993459a04
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-10-21 20:24:14 -0400

    Display full name, email of user in profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import Layout from "../components/Layout";
 
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Profile() {
+  const { currentUser } = useAuth();
 
   return (
     <Layout>
-      <div>Profile Page</div>
+        <div>
+            <h1>Profile</h1>
+            <p><strong>Full Name:</strong> {currentUser.displayName}</p>
+            <p><strong>Email:</strong> {currentUser.email}</p>
+        </div>
     </Layout>
   );
 }
