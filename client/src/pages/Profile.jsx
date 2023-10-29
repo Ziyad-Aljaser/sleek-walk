f8e29c7b93e8fd0011936adf551d7906ee1683f4
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-10-28 20:13:05 -0400

    Style sections on profile page for user info

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-
 import { useAuth } from "../contexts/AuthContext";
 
 export default function Profile() {
@@ -9,11 +8,32 @@ export default function Profile() {
 
   return (
     <Layout>
-        <div>
-            <h1>Profile</h1>
-            <p><strong>Full Name:</strong> {currentUser.displayName}</p>
-            <p><strong>Email:</strong> {currentUser.email}</p>
+      <div className="flex items-center justify-center min-h-screen bg-base-300 p-4 -mt-12">
+        <div className="card w-full max-w-4xl bg-base-100 shadow-xl">
+          <div className="card-body">
+
+            {/* Profile Header */}
+            <h1 className="text-4xl font-bold text-center mb-8">Profile</h1>
+
+            <div className="flex">
+              {/* Vertical Buttons */}
+              <div className="join join-vertical mr-8 bg-base-200">
+                <button className="btn join-item py-14">Name</button>
+                <button className="btn join-item py-14">Email</button>
+                <button className="btn join-item py-14">Address</button>
+              </div>
+              
+              {/* Profile Information */}
+              <div className="flex-grow">
+                <p><strong>Full Name:</strong> {currentUser.displayName}</p>
+                <p><strong>Email:</strong> {currentUser.email}</p>
+              </div>
+
+            </div>
+
+          </div>
         </div>
+      </div>
     </Layout>
   );
 }
