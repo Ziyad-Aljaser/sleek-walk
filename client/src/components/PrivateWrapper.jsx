2b1bf1a8a22410790b6de71c25c028a2688a313c
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-08 09:54:56 -0500

    Refactor PrivateWrapper component to include separate wrappers for user and admin roles

--- client/src/components/PrivateWrapper.jsx
+++ client/src/components/PrivateWrapper.jsx
@@ -1,11 +1,46 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useEffect, useState } from "react";
+
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { getUserRole } from "../utils/FirestoreUtils";
+import { db } from "../config/firebase";
 
 // Used to protect the path from users that are not logged in. It redirect the user to the login page
-const PrivateWrapper = ({ children }) => {
+export const PrivateUserWrapper = ({ children }) => {
   const { currentUser } = useAuth();
 
   return currentUser ? children : <Navigate to="/login" replace />;
 };
 
-export default PrivateWrapper;
+// Used to protect the path from users that are not logged in as admin. It redirect the user to the home page
+export const PrivateAdminWrapper = ({ children }) => {
+  const { currentUser } = useAuth();
+  const userId = currentUser?.uid;
+
+  const [userRole, setUserRole] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const role = await getUserRole(userId, db);
+        setUserRole(role);
+      } catch (error) {
+        console.error("Error fetching user role: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserRole();
+  }, [userId]); // adding userId to dependency array to re-run effect when it changes
+
+  // Check if the user is an admin
+  const isAdmin = userRole === "Admin";
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAdmin ? children : <Navigate to="/" replace />;
+};
