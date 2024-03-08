5b3ad75eec82b6b0826c1b961b4cedb7f27e9731
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-08 16:13:30 -0500

    Refactor AdminDashboard component with the needed fields

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,67 +1,114 @@
-import React from 'react';
+import React from "react";
 import Layout from "../components/Layout/Layout";
 
 export default function AdminDashboard() {
-    
-const handleLogin = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
     console.log("AdminDashboard");
-}
-const handleForm = (e) => {
+  };
+  const handleForm = (e) => {
     console.log("AdminDashboard submit form");
-}
+  };
 
+  return (
+    <Layout>
+      <div className="flex items-center justify-center h-screen bg-base-300 p-5">
+        <div className="card w-full max-w-3xl bg-base-100 shadow-xl -mt-24">
+          <div className="card-body">
+            <h1 className="text-4xl font-bold text-center mb-12">
+              Add New Shoes
+            </h1>
 
-    return (
-        <Layout>
-            <div className="flex items-center justify-center h-screen bg-base-300 p-5">
-                <div className="card w-full max-w-md bg-base-100 shadow-xl -mt-24">
-                    <div className="card-body">
-                        <h1 className="text-4xl font-bold text-center mb-6">Add New Shoes</h1>
-
-                        <form onSubmit={handleLogin}>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    placeholder="Email"
-                                    className="input input-bordered input-primary"
-                                    onChange={handleForm}
-                                    required
-                                />
-                            </div>
-
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    className="input input-bordered input-primary"
-                                    onChange={handleForm}
-                                    required
-                                />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">
-                                        Forgot password?
-                                    </a>
-                                </label>
-                            </div>
-
-                            <div className="form-control mt-8">
-                                <button type="submit" className="btn btn-primary">
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+            <form onSubmit={handleLogin} className="flex flex-wrap">
+              <div className="w-full md:w-1/2 px-6">
+                {/* Left Column */}
+                <div className="form-control mb-6">
+                  <label className="label">
+                    <span className="label-text font-bold">Title</span>
+                  </label>
+                  <input
+                    className="input input-bordered input-primary"
+                    onChange={handleForm}
+                    required
+                  />
                 </div>
-            </div>
-        </Layout>
-    );
+
+                <div className="form-control mb-6">
+                  <label className="label">
+                    <span className="label-text font-bold">Type</span>
+                  </label>
+                  <select
+                    className="select select-bordered select-primary"
+                    onChange={handleForm}
+                    required
+                  >
+                    <option value="">Please select</option>
+                    <option value="Men">Men</option>
+                    <option value="Women">Women</option>
+                    <option value="Children">Children</option>
+                  </select>
+                </div>
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-bold">Image</span>
+                  </label>
+                  <input
+                    type="file"
+                    className="file-input file-input-bordered"
+                    required
+                  />
+                </div>
+              </div>
+
+              {/* Right Column */}
+              <div className="w-full md:w-1/2 px-6">
+                <div className="form-control mb-6">
+                  <label className="label">
+                    <span className="label-text font-bold">Category</span>
+                  </label>
+                  <select
+                    className="select select-bordered select-primary"
+                    onChange={handleForm}
+                    required
+                  >
+                    <option value="">Please select</option>
+                    <option value="Athletic">Athletic</option>
+                    <option value="Casual">Casual</option>
+                    <option value="Dress">Dress</option>
+                  </select>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-bold">Price</span>
+                  </label>
+                  <input
+                    type="number"
+                    className="input input-bordered input-primary"
+                    onChange={handleForm}
+                    required
+                    step="1" // Allows only integer values
+                    max="9999" // Maximum value of 9999
+                  />
+                </div>
+              </div>
+
+              {/* Used for the add button */}
+              <div className="w-full flex justify-center mt-12 py-6">
+                {" "}
+                {/* Centering Container */}
+                <div className="form-control w-1/2">
+                  {" "}
+                  {/* Button's Container */}
+                  <button type="submit" className="btn btn-primary w-full">
+                    Add
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
