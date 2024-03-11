32f38aec6df4e6337318c4e55fdf8ac2a668cce2
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-11 18:50:19 -0400

    Update Navbar dropdown menu styling

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -295,7 +295,7 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 top-[125%]"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               {currentUser ? ( // Conditional rendering based on currentUser
                 <>
