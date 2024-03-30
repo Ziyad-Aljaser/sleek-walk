1e94fb70c989ae35d559dd5f1f5a5229475aa2bd
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-30 12:13:34 -0400

    Refactor Navbar component to conditionally render cart section based on user authentication status

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -232,48 +232,49 @@ const Navbar = () => {
             )}
           </div>
 
-          {/* Cart */}
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-7 w-7"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                >
-                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-                <span className="badge badge-sm indicator-item">
-                  {itemCount}
-                </span>
-              </div>
-            </label>
-            <div
-              tabIndex={0}
-              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-            >
-              <div className="card-body">
-                <span className="font-bold text-lg">{itemCount} Items</span>
-                <span className="text-info">
-                  Subtotal: ${subtotal.toFixed(2)}
-                </span>
-                <div className="card-actions">
-                  <Link
-                    to="/cart"
-                    className="btn btn-primary btn-block"
-                    onClick={handleButtonClick}
+          {currentUser && (
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7 w-7"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                   >
-                    View cart
-                  </Link>
+                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">
+                    {itemCount}
+                  </span>
+                </div>
+              </label>
+              <div
+                tabIndex={0}
+                className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg">{itemCount} Items</span>
+                  <span className="text-info">
+                    Subtotal: ${subtotal.toFixed(2)}
+                  </span>
+                  <div className="card-actions">
+                    <Link
+                      to="/cart"
+                      className="btn btn-primary btn-block"
+                      onClick={handleButtonClick}
+                    >
+                      View cart
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
 
           {/* User */}
           <div className="dropdown dropdown-end">
