b91314208062f15de9c0b4c0eee0aaf0f5a9c3bb
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-30 18:40:13 -0400

    Fixed a bug in the login functionality and added support for phone-sized carts.

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -22,7 +22,6 @@ const Navbar = () => {
 
   const { shoes } = useShoesData();
 
-
   // Used for the light/dark mode
   // use theme from local storage if available or set light theme
   const [theme, setTheme] = useState(
@@ -91,7 +90,6 @@ const Navbar = () => {
   };
 
   // --------------- Used to display the items count/subtotal ---------------
-  // const userId = "UserID";
   const userId = currentUser?.uid;
   const itemCount = useItemCount(userId, db);
   const subtotal = useSubtotal(userId, db);
@@ -161,9 +159,39 @@ const Navbar = () => {
                 <div className="form-control mt-3">
                   <input
                     type="text"
-                    placeholder="Search shoes..."
+                    placeholder="Search Shoes..."
                     className="input input-bordered input-primary w-full"
+                    value={query}
+                    onChange={handleSearch}
                   />
+                  {/* Display Search Results */}
+                  {query && (
+                    <ul
+                      tabIndex={0}
+                      className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full absolute top-full"
+                    >
+                      {filteredShoes.length > 0 ? (
+                        filteredShoes.slice(0, 3).map((shoe) => (
+                          <li key={shoe._id} className="my-2">
+                            <Link
+                              to={`/shoes-details/${shoe._id}`}
+                              onClick={resetSearch}
+                            >
+                              <img
+                                src={shoe.image}
+                                alt={shoe.title}
+                                width="60"
+                                className="rounded-lg"
+                              />
+                              <h3>{shoe.title}</h3>
+                            </Link>
+                          </li>
+                        ))
+                      ) : (
+                        <li className="my-2 text-center">No results found</li>
+                      )}
+                    </ul>
+                  )}
                 </div>
               </li>
             </ul>
@@ -192,9 +220,9 @@ const Navbar = () => {
         </div>
 
         {/* Search-Cart-User Section */}
-        <div className="navbar-end hidden lg:flex ">
+        <div className="navbar-end">
           {/* Search */}
-          <div className="form-control p-2 ">
+          <div className="form-control p-2 hidden lg:flex">
             <input
               type="text"
               placeholder="Search Shoes..."
@@ -232,6 +260,7 @@ const Navbar = () => {
             )}
           </div>
 
+          {/* Cart */}
           {currentUser && (
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle">
@@ -277,99 +306,101 @@ const Navbar = () => {
           )}
 
           {/* User */}
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
+          <div className="hidden lg:flex">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                >
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                  <path d="M16 7 A4 4 0 0 1 12 11 A4 4 0 0 1 8 7 A4 4 0 0 1 16 7 z" />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-                <path d="M16 7 A4 4 0 0 1 12 11 A4 4 0 0 1 8 7 A4 4 0 0 1 16 7 z" />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {currentUser ? ( // Conditional rendering based on currentUser
-                <>
-                  <li className="menu-title">
-                    <span className="text-purple-500">
-                      Welcome, {currentUser.displayName}!
-                    </span>
-                  </li>
-                  {userRole === "Admin" ? (
-                    <>
-                      <li className="mt-2">
-                        <Link to="/admin-dashboard">Admin Dashboard</Link>
-                      </li>
-                    </>
-                  ) : (
-                    <>
-                      <li className="mt-2">
-                        <Link to="/profile">Profile</Link>
-                      </li>
-                    </>
-                  )}
+                {currentUser ? ( // Conditional rendering based on currentUser
+                  <>
+                    <li className="menu-title">
+                      <span className="text-purple-500">
+                        Welcome, {currentUser.displayName}!
+                      </span>
+                    </li>
+                    {userRole === "Admin" ? (
+                      <>
+                        <li className="mt-2">
+                          <Link to="/admin-dashboard">Admin Dashboard</Link>
+                        </li>
+                      </>
+                    ) : (
+                      <>
+                        <li className="mt-2">
+                          <Link to="/profile">Profile</Link>
+                        </li>
+                      </>
+                    )}
 
-                  <div className="divider -mt-0.5"></div>
-                  {/* - before mt is for forcing the button to go up */}
-                  <li className="-mt-5">
-                    <button onClick={handleLogout}>Logout</button>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <Link to="/login">Log in</Link>
-                  </li>
-                  <li>
-                    <Link to="/signup">Sign up</Link>
-                  </li>
-                </>
-              )}
-            </ul>
+                    <div className="divider -mt-0.5"></div>
+                    {/* - before mt is for forcing the button to go up */}
+                    <li className="-mt-5">
+                      <button onClick={handleLogout}>Logout</button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <Link to="/login">Log in</Link>
+                    </li>
+                    <li>
+                      <Link to="/signup">Sign up</Link>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
           </div>
-        </div>
 
-        {/* Light/Dark Mode Section */}
-        <div className="flex justify-end flex-1 px-2">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <label className="swap swap-rotate">
-              <input
-                type="checkbox"
-                onChange={handleToggle}
-                // show toggle image based on localstorage theme
-                checked={theme === "light" ? false : true}
-              />
-              {/* this hidden checkbox controls the state */}
-              <input type="checkbox" />
+          {/* Light/Dark Mode Section */}
+          <div className="flex justify-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <label className="swap swap-rotate">
+                <input
+                  type="checkbox"
+                  onChange={handleToggle}
+                  // show toggle image based on localstorage theme
+                  checked={theme === "light" ? false : true}
+                />
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
 
-              {/* sun icon */}
-              <svg
-                className="swap-on fill-current w-8 h-8"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-              </svg>
+                {/* sun icon */}
+                <svg
+                  className="swap-on fill-current w-8 h-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                </svg>
 
-              {/* moon icon */}
-              <svg
-                className="swap-off fill-current w-8 h-8"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-              </svg>
+                {/* moon icon */}
+                <svg
+                  className="swap-off fill-current w-8 h-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                </svg>
+              </label>
             </label>
-          </label>
+          </div>
         </div>
       </div>
     </div>
