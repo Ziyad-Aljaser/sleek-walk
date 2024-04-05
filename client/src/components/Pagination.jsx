d00e16563c19a3116ebd1d6ba013c49bf805b8ef
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-05 11:11:19 -0400

    Refactor pagination component

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Pagination = ({ totalPages, currentPage, setCurrentPage, handleButtonClick }) => {
+    return (
+      <div className="join flex justify-center mb-5">
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index + 1}
+          className={`join-item btn text-2xl shadow-xl ${
+            currentPage === index + 1 ? "btn-active" : ""
+          }`}
+          onClick={() => {
+            setCurrentPage(index + 1);
+            handleButtonClick();
+          }}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+    );
+  };
+
+export default Pagination;
