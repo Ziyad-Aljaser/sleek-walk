79f82ad990260fc775bd11763b54b05624f6adc5
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-05-23 18:41:06 +0300

    Add icons

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -56,40 +56,61 @@ export default function Login() {
             <h1 className="text-4xl font-bold text-center mb-6">Login</h1>
 
             {error ? (
-              <div className="alert alert-error">
+              <div className="alert alert-error mb-4">
                 <span>Error! {error}</span>
               </div>
             ) : (
               loginSuccess && (
-                <div className="alert alert-success">
+                <div className="alert alert-success mb-4">
                   <span>Login successful! Welcome Back!</span>
                 </div>
               )
             )}
             <form onSubmit={handleLogin}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
+              {/* Email Section */}
+              <div className="form-control mb-8 relative">
+                <div className="icon-wrapper absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-6 h-6 opacity-70"
+                  >
+                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                  </svg>
+                </div>
                 <input
-                  type="email"
+                  type="text"
                   name="email"
                   placeholder="Email"
-                  className="input input-bordered input"
+                  className="input input-bordered w-full pl-10 text-lg"
                   onChange={handleForm}
                   required
                 />
               </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
+              {/* Password Section */}
+              <div className="form-control mb-8 relative">
+                <div className="icon-wrapper absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-6 h-6 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
                 <input
                   type="password"
                   name="password"
                   placeholder="Password"
-                  className="input input-bordered input"
+                  className="input input-bordered w-full pl-10 text-lg"
                   onChange={handleForm}
                   required
                 />
@@ -98,7 +119,7 @@ export default function Login() {
               <div className="form-control mt-8">
                 <button
                   type="submit"
-                  className="btn btn-primary"
+                  className="btn btn-primary text-lg"
                   disabled={isLoading}
                 >
                   {isLoading ? (
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import { useAuth } from "../hooks/useAuth";
 
 import Layout from "../components/Layout/Layout";
@@ -63,71 +62,115 @@ export default function Signup() {
       <div className="flex items-center justify-center h-screen bg-base-300 p-5">
         <div className="card w-full max-w-md glass shadow-xl">
           <div className="card-body">
-            <h1 className="text-4xl font-bold text-center mb-6">New Account</h1>
+            <h1 className="text-4xl font-bold text-center mb-10">
+              New Account
+            </h1>
             {/* Used to show the Error/Success notification to the user */}
             {error ? (
-              <div className="alert alert-error">
+              <div className="alert alert-error mb-4">
                 <span>Error! {error}</span>
               </div>
             ) : (
               signupSuccess && (
-                <div className="alert alert-success">
+                <div className="alert alert-success mb-4">
                   <span>Signup Successful, Welcome!</span>
                 </div>
               )
             )}
             <form onSubmit={handleSubmit}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Full Name</span>
-                </label>
+              {/* Full Name Section */}
+              <div className="form-control mb-8 relative">
+                <div className="icon-wrapper absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-6 h-6 opacity-70"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                  </svg>
+                </div>
+
                 <input
                   type="text"
                   name="fullName"
                   placeholder="Full Name"
-                  className="input input-bordered input"
+                  className="input input-bordered w-full pl-10 text-lg"
                   onChange={handleForm}
                   required
                 />
               </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
+              {/* Email Section */}
+              <div className="form-control mb-8 relative">
+                <div className="icon-wrapper absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-6 h-6 opacity-70"
+                  >
+                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                  </svg>
+                </div>
                 <input
-                  type="email"
+                  type="text"
                   name="email"
                   placeholder="Email"
-                  className="input input-bordered input"
+                  className="input input-bordered w-full pl-10 text-lg"
                   onChange={handleForm}
                   required
                 />
               </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
+              {/* Password Section */}
+              <div className="form-control mb-8 relative">
+                <div className="icon-wrapper absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-6 h-6 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
                 <input
                   type="password"
                   name="password"
                   placeholder="Password"
-                  className="input input-bordered input"
+                  className="input input-bordered w-full pl-10 text-lg"
                   onChange={handleForm}
                   required
                 />
               </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Confirm Password</span>
-                </label>
+              {/* Confirm Password Section */}
+              <div className="form-control mb-8 relative">
+                <div className="icon-wrapper absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-6 h-6 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
                 <input
                   type="password"
                   name="passwordConfirm"
                   placeholder="Confirm Password"
-                  className="input input-bordered input"
+                  className="input input-bordered w-full pl-10 text-lg"
                   onChange={handleForm}
                   required
                 />
@@ -136,7 +179,7 @@ export default function Signup() {
               <div className="form-control mt-8">
                 <button
                   type="submit"
-                  className="btn btn-primary"
+                  className="btn btn-primary text-lg"
                   disabled={isLoading}
                 >
                   {isLoading ? (
